f9398868c7f053bc0984ae5e7ebea390c86d293e
zhangliu <2351186624@qq.com>
2021-05-13 08:50:30 +0800

    测试

--- src/api/data.js
+++ src/api/data.js
@@ -49,3 +49,10 @@ export const getTreeSelectData = () => {
     method: 'get'
   })
 }
+
+export const getTreeSelectData1 = () => {
+  return axios.request({
+    url: 'get_tree_select_data',
+    method: 'get'
+  })
+}
